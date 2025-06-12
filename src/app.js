614de686deabc722d5beefaaa6798aeced4c3a60
GU1LL3_L4-C0 <guillermo96lc@hotmail.com>
2025-06-12 19:49:34 -0300

    metodos de products

--- src/app.js
+++ src/app.js
@@ -13,54 +13,8 @@ app.engine('handlebars', handlebars.engine());
 app.set('views', __dirname + "/views/");
 app.set('view engine', 'handlebars');
 
-//Ejercicio
-/* const users = [
-  {
-    nombre: "Lucía",
-    apellido: "Gómez",
-    edad: 28,
-    correo: "lucia.gomez@example.com",
-    telefono: "+54 11 2345 6789"
-  },
-  {
-    nombre: "Martín",
-    apellido: "Pérez",
-    edad: 35,
-    correo: "martin.perez@example.com",
-    telefono: "+54 11 3456 7890"
-  },
-  {
-    nombre: "Camila",
-    apellido: "Rodríguez",
-    edad: 24,
-    correo: "camila.rodriguez@example.com",
-    telefono: "+54 11 4567 8901"
-  },
-  {
-    nombre: "Julián",
-    apellido: "Fernández",
-    edad: 41,
-    correo: "julian.fernandez@example.com",
-    telefono: "+54 11 5678 9012"
-  },
-  {
-    nombre: "Sofía",
-    apellido: "López",
-    edad: 30,
-    correo: "sofia.lopez@example.com",
-    telefono: "+54 11 6789 0123"
-  }
-]; */
-/* app.get("/test", (req,res) => {
-    let randomNum = Math.floor(Math.random() * users.length)
-    res.render('index', users[randomNum])
-}) */
-
-
-app.get("/saludo", (req,res) =>{
-    res.send('Bienvenidos')
-})
-
 app.use("/", productsRouter)
+
+app.use("/", cartsRouter)
 
 app.listen(PORT, ()=> console.log(`Server on port: ${PORT}`));
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -3,7 +3,7 @@ const router = Router()
 
 const products = [
     {
-        id:1,
+        id: 1,
         title: "Casablanca",
         description: "Casablanca es una historia de amor y sacrificio ambientada durante la Segunda Guerra Mundial.",
         code: "",
@@ -11,10 +11,12 @@ const products = [
         status: true,
         stock: 10,
         category: "Drama",
-        thumbnails: []
+        thumbnails: [
+            "https://http2.mlstatic.com/D_NQ_NP_735864-MLA74159033785_012024-O.webp"
+        ]
     },
     {
-        id:2,
+        id: 2,
         title: "Psycho",
         description: "Psycho es un thriller psicológico sobre un asesinato en un motel que revela una oscura doble identidad.",
         code: "",
@@ -22,32 +24,38 @@ const products = [
         status: true,
         stock: 7,
         category: "Terror",
-        thumbnails: []
+        thumbnails: [
+            "https://atthemovies.uk/cdn/shop/products/psycho1965os2650LB.jpg?v=1621382516"
+        ]
     },
     {
-        id:3,
+        id: 3,
         title: "Metropolis",
         description: "Metrópolis es una obra de ciencia ficción que retrata una ciudad futurista dividida entre una élite dominante y trabajadores oprimidos, explorando la lucha por la justicia social.",
         code: "",
         price: 2700,
-        status: false,
+        status: true,
         stock: 3,
         category: "Sci-fi",
-        thumbnails: []
+        thumbnails: [
+            "https://http2.mlstatic.com/D_913637-MLA26668029558_012018-O.jpg"
+        ]
     },
     {
-        id:4,
+        id: 4,
         title: "Django",
         description: "Django es un wéstern violento y estilizado sobre un solitario pistolero que busca venganza en un mundo brutal y corrupto.",
         code: "",
         price: 1500,
-        status: false,
+        status: true,
         stock: 2,
         category: "Western",
-        thumbnails: []
+        thumbnails: [
+            "https://cdn.posteritati.com/posters/000/000/045/928/django-md-web.jpg"
+        ]
     },
     {
-        id:5,
+        id: 5,
         title: "Se7en",
         description: "Se7en es un oscuro thriller policial donde dos detectives persiguen a un asesino en serie que comete crímenes basados en los siete pecados capitales.",
         code: "",
@@ -55,13 +63,16 @@ const products = [
         status: true,
         stock: 4,
         category: "Thriller",
-        thumbnails: []
+        thumbnails: [
+            "https://image.tmdb.org/t/p/original/aVNqeE2JjiW7VMSmkxPdN0Xd5xo.jpg"
+        ]
     }
-]
+];
+
 
 //GET: Todos los productos
 router.get("/", (req,res) => {
-    res.send({status: "Succes", payload: products})
+    res.send({status: "Success", payload: products})
 })
 
 //GET: Por su ID
@@ -78,28 +89,48 @@ router.get("/:productId", (req,res) => {
 //POST
 router.post("/", (req,res) => {
     let prod = req.body;
-    prod.id = Math.floor(Math.random() * 100 + 1);
-    if(!prod.title || !prod.description) {
-        return res.status(400).send({status: "Error", message: "Formato invalido"})
+
+    if(!prod.title || !prod.description || !prod.code || !prod.price || !prod.status || !prod.stock || !prod.category) {
+        return res.status(400).send({status: "Error", message: "Campos incompletos"})
     }
+    prod.id = Math.floor(Math.random() * 100 + 1);
+
     products.push(prod)
-    res.send({status: "Succes", payload: `Agregado el film con el id: ${prod.id}`})
+
+    res.send({status: "Success", payload: `Agregado el film con el id: ${prod.id}`})
 })
 
 //PUT
 router.put("/:productId", (req,res) => {
 
-    let { productId } = req.params;
-
+    const { productId } = req.params;
     let prodUpdate = req.body;
-
     let productPosition = products.findIndex(p => p.id === parseInt(productId))
 
-    if(productPosition > 0) {
+    if(productPosition === -1) {
+        return res.status(404).send({status: "Error", message: "Producto no encontrado"})
+    }
+    prodUpdate.id = products[productPosition].id
+    products[productPosition] = prodUpdate;
+
+    res.send({ status: "Success", payload: prodUpdate });
+})
+
+//DELETE
+router.delete("/:productId", (req,res) => {
+
+    let { productId } = req.params;
+    productId = parseInt(productId);
+
+    let productPosition = products.findIndex(p => p.id === productId)
+
+    if(productPosition < 0) {
         return res.send({status: "Error", message: "Producto no encontrado"})
     }
-    prodUpdate.id = products[productPosition.id]
-    products[productPosition] = prodUpdate;
+    products.splice(productPosition, 1)
+
+    res.send({status: "Success", message: `Producto con el id ${productId} eliminado exitosamente.`})
+
 })
 
 export default router;

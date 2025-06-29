3e4c95aa4e033944f2e9c518aae221e4b4371355
GU1LL3_L4-C0 <guillermo96lc@hotmail.com>
2025-06-29 12:58:15 -0300

    carts

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cartsRouter from './routes/carts.products.js'
 import productsRouter from './routes/products.router.js'
 import handlebars from 'express-handlebars'
 import __dirname from './utils.js'
+import fs from 'fs'
 
 const app = express();
 const PORT = 8080;
@@ -14,6 +15,16 @@ app.use(express.urlencoded({extended: true }));
 app.engine('handlebars', handlebars.engine());
 app.set('views', __dirname + "/views/");
 app.set('view engine', 'handlebars');
+
+function leerProductos() {
+  const data = fs.readFileSync('./data/products.json', 'utf-8');
+  return JSON.parse(data);
+}
+
+app.get('/home',(req,res) => {
+    const products = leerProductos();
+    res.render("home",products) //1:08
+})
 
 //Rutas
 app.use("/api", productsRouter)
--- src/routes/carts.products.js
+++ src/routes/carts.products.js
@@ -1,10 +1,37 @@
 import { Router } from "express";
 const router = Router()
 import fs from 'fs';
-const rutaArchivo = './newFile.txt'
+import __dirname from "../utils.js";
+
+//Variables constantes
+const rutaArchivo = `${__dirname}/data/carts.json`
+const codeFormat = 'utf-8'
+
+console.log(rutaArchivo);
+
+
+function leerCarrito() {
+    try {
+        const cartData = fs.readFileSync(rutaArchivo,codeFormat);
+        return JSON.parse(cartData)
+    } catch (error) {
+        console.error("❌ Error al leer archivo:", error.message);
+        return [];
+    }
+}
+
+function guardarCarrito(data) {
+    try {
+        fs.writeFileSync(rutaArchivo,JSON.stringify(data, null, 2),codeFormat);
+        return true;
+    } catch (error) {
+        console.error("❌ Error al guardar archivo:", error.message);
+        return false;
+    }
+}
 
 //POST
-router.post("/", (req,res) => {
+router.post("/createCart", (req,res) => {
     let cartId = Math.floor(Math.random() * 100 + 1)
     let products = [];
 
@@ -13,7 +40,49 @@ router.post("/", (req,res) => {
         productsArray: products
     }
 
-    res.send(carrito)
+    let carts = leerCarrito();
+
+    carts.push(carrito)
+    
+    let exito = guardarCarrito(carts)
+
+    if (exito) {
+        res.send({
+            status: "Success",
+            payload: carrito
+        });
+    } else {
+        res.status(500).send({
+            status: "Error",
+            message: "No se pudo guardar el carrito."
+        });
+    }
+})
+
+//GET todos
+router.get("/carts", (req,res) => {
+    const carts = leerCarrito();
+
+    console.log(carts);
+
+    res.json(carts);
+})
+
+//GET por id
+router.get("/getcart/:cartId", (req,res) => {
+    let { cartId } = req.params;
+    cartId = parseInt(cartId);
+
+    const cartData = leerCarrito();
+
+    let cartPorId = cartData.find(c => c.id === cartId)
+
+    if(!cartData) {
+        return res.status(404).send({ status: "Error", message: `Carrito con el id: ${cartId} no encontrado.` });
+    }
+
+    res.send({status: "Success", payload: cartPorId})
+
 })
 
 export default router;

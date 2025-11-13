d4a7f45f7132bd05463aeae7790ce0d8f34cf4f6
GU1LL3_L4-C0 <guillermo96lc@hotmail.com>
2025-11-13 19:54:34 -0300

    + supertest

--- test/supertest.test.js
+++ test/supertest.test.js
@@ -1,7 +1,6 @@
 import * as chai from 'chai'
 import { faker } from '@faker-js/faker'
 import supertest from 'supertest'
-import mongoose from 'mongoose'
 import { logger } from '../src/config/logger.js';
 
 const expect = chai.expect;
@@ -10,7 +9,7 @@ const requester = supertest(urlServer);
 
 describe('Testing de APIs', () => {
 
-    describe('test de usuarios', () => {
+    describe('Test de usuarios', () => {
         
         it('Recibir todos los usuarios', async () => {
             const {statusCode,_body} = await requester.get('/api/users/all')
@@ -66,6 +65,71 @@ describe('Testing de APIs', () => {
             const {statusCode} = await requester.post('/api/users/createuser').send(fakeUser3)
             logger.info(statusCode)
             expect(statusCode).is.eqls(400)
+        })
+    })
+
+    describe('Test de productos', () => {
+        it('Recibir todos los productos en un array', async () => {
+            const {statusCode,_body} = await requester.get('/api/')
+
+            expect(statusCode).is.eqls(200)
+
+            expect(_body.payload).to.be.an('array')
+
+        })
+
+        it('Crear un producto', async () => {
+            const fakeProduct = {
+                title: faker.lorem.words(3),
+                description: faker.lorem.sentences(2),
+                code: faker.string.alphanumeric(8).toUpperCase(),
+                price: faker.number.int({ min: 500, max: 5000 }),
+                status: faker.datatype.boolean(),
+                stock: faker.number.int({ min: 0, max: 100 }),
+                category: faker.helpers.arrayElement([
+                    "Acción",
+                    "Comedia",
+                    "Drama",
+                    "Terror",
+                    "Ciencia Ficción",
+                    "Aventura",
+                    "Animación",
+                    "Suspenso"
+                ]),
+                thumbnails: faker.image.url()
+            }
+
+            const {statusCode,_body} = await requester.post('/api/create').send(fakeProduct)
+
+            expect(statusCode).is.eqls(201)
+            logger.info(statusCode)
+            expect(_body.payload).is.ok.and.is.to.have.property('_id')
+        })
+
+        it('Crear producto sin titulo', async () => {
+            const fakeProduct2 = {
+                description: faker.lorem.sentences(2),
+                code: faker.string.alphanumeric(8).toUpperCase(),
+                price: faker.number.int({ min: 500, max: 5000 }),
+                status: faker.datatype.boolean(),
+                stock: faker.number.int({ min: 0, max: 100 }),
+                category: faker.helpers.arrayElement([
+                    "Acción",
+                    "Comedia",
+                    "Drama",
+                    "Terror",
+                    "Ciencia Ficción",
+                    "Aventura",
+                    "Animación",
+                    "Suspenso"
+                ]),
+                thumbnails: faker.image.url()
+            }
+
+            const {statusCode} = await requester.post('/api/create').send(fakeProduct2)
+
+            expect(statusCode).is.eqls(400)
+            logger.info(statusCode)
         })
     })
 })
